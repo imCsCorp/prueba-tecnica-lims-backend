be2f2a6a318d3a197da1945c571a153435fb4755
Camilo Alejandro Soto Vega <camilo.alejandro.soto.v@gmail.com>
2024-05-02 18:16:37 -0500

    auth controller

---
+++ src/main/java/com/camilosoto/pruebatecnica/domain/dto/LoginDTO.java
@@ -0,0 +1,12 @@
+package com.camilosoto.pruebatecnica.domain.dto;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class LoginDTO {
+    private String email;
+    private String password;
+}
---
+++ src/main/java/com/camilosoto/pruebatecnica/web/controller/AuthController.java
@@ -0,0 +1,27 @@
+package com.camilosoto.pruebatecnica.web.controller;
+
+import com.camilosoto.pruebatecnica.domain.dto.LoginDTO;
+import com.camilosoto.pruebatecnica.domain.dto.UserDTO;
+import com.camilosoto.pruebatecnica.domain.service.AuthService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    private AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<Void> login(@RequestBody LoginDTO loginDTO) {
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<UserDTO> register(@RequestBody UserDTO user) {
+        return new ResponseEntity<>(authService.create(user), HttpStatus.CREATED);
+    }
+}
