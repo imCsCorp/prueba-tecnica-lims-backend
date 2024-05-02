9e151626f5e68dbd4482be540f597cf7f180ffbc
Camilo Alejandro Soto Vega <camilo.alejandro.soto.v@gmail.com>
2024-05-02 17:40:45 -0500

    adding mapper

---
+++ src/main/java/com/camilosoto/pruebatecnica/persistence/mapper/UserMapper.java
@@ -0,0 +1,26 @@
+package com.camilosoto.pruebatecnica.persistence.mapper;
+import com.camilosoto.pruebatecnica.domain.dto.UserDTO;
+import com.camilosoto.pruebatecnica.persistence.entity.User;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+    @Mappings({
+            @Mapping(source = "id", target = "id"),
+            @Mapping(source = "firstName", target = "firstName"),
+            @Mapping(source = "lastName", target = "lastName"),
+            @Mapping(source = "email", target = "email"),
+            @Mapping(source = "password", target = "password"),
+            @Mapping(source = "state", target = "active"),
+    })
+    UserDTO toUserDTO(User user);
+    List<UserDTO> toUsersDTO(List<User> users);
+
+    @InheritInverseConfiguration
+    User toUser(UserDTO userDTO);
+}
