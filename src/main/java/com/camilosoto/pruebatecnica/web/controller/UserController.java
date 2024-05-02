80c0176eb4b514fbdac5426e9357f021d08ba762
Camilo Alejandro Soto Vega <camilo.alejandro.soto.v@gmail.com>
2024-05-02 18:06:16 -0500

    userController

--- src/main/java/com/camilosoto/pruebatecnica/web/controller/UserController.java
+++ src/main/java/com/camilosoto/pruebatecnica/web/controller/UserController.java
@@ -1,2 +1,38 @@
-package com.camilosoto.pruebatecnica.web.controller;public class UserController {
+package com.camilosoto.pruebatecnica.web.controller;
+
+import com.camilosoto.pruebatecnica.domain.dto.UserDTO;
+import com.camilosoto.pruebatecnica.domain.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/id/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable("id") int userId){
+        return userService.findById(userId)
+                .map(u-> new ResponseEntity<>(u, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<UserDTO> update(@RequestBody UserDTO user){
+        return new ResponseEntity<>(userService.create(user), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> delete(@PathVariable("id") int userId){
+        if(userService.remove(userId)){
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
 }
