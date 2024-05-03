25d3026c7d7656fc9170e505508c82f2a9c3cd1d
Camilo Alejandro Soto Vega <camilo.alejandro.soto.v@gmail.com>
2024-05-03 05:10:02 -0500

    cors config

--- src/main/java/com/camilosoto/pruebatecnica/web/config/CorsConfig.java
+++ src/main/java/com/camilosoto/pruebatecnica/web/config/CorsConfig.java
@@ -6,16 +6,18 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class CorsConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource (){
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
-        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
-        corsConfiguration.setAllowedOriginPatterns(Arrays.asList("*"));
+        corsConfiguration.setAllowedOriginPatterns(List.of("*")); // Permitir cualquier origen (con credentials, usa allowedOriginPatterns)
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); // Permitir métodos permitidos
+        corsConfiguration.setAllowedHeaders(List.of("*")); // Permitir todos los encabezados
+        corsConfiguration.setExposedHeaders(List.of("Authorization")); // Exponer encabezados personalizados
+        corsConfiguration.setAllowCredentials(true); // Permitir el intercambio de credenciales
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", corsConfiguration);
