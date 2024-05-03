a47794e426546cc31a69771a3469420baa4406d9
Camilo Alejandro Soto Vega <camilo.alejandro.soto.v@gmail.com>
2024-05-03 04:26:43 -0500

    password management in user service

--- src/main/java/com/camilosoto/pruebatecnica/domain/service/UserService.java
+++ src/main/java/com/camilosoto/pruebatecnica/domain/service/UserService.java
@@ -3,6 +3,7 @@ package com.camilosoto.pruebatecnica.domain.service;
 import com.camilosoto.pruebatecnica.domain.dto.UserDTO;
 import com.camilosoto.pruebatecnica.persistence.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,6 +13,8 @@ import java.util.Optional;
 public class UserService {
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public UserDTO update(UserDTO user){
         return userRepository.save(user);
@@ -30,5 +33,9 @@ public class UserService {
             userRepository.delete(userId);
             return true;
         }).orElse(false);
+    }
+
+    public String encryptPassword(String password) {
+        return passwordEncoder.encode(password);
     }
 }
